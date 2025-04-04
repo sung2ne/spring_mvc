2907206d5cadb21a786110789aa6d0fa2b442e91
정필성 <ibetter.kr@gmail.com>
2025-04-04 13:51:54 +0900

    PART 04-05 완료

---
+++ part04-05/src/main/java/com/example/spring/post/PostController.java
@@ -0,0 +1,34 @@
+package com.example.spring.post;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+public class PostController {
+
+    @Autowired
+    PostService postService;
+
+    // 게시글 등록 (화면, GET)
+    @RequestMapping(value = "/posts/create", method = RequestMethod.GET)
+    public String createGet() {
+        return "post/create";
+    }
+
+    // 게시글 등록 (처리, POST)
+    @RequestMapping(value = "/posts/create", method = RequestMethod.POST)
+    public String createPost(PostDto post, RedirectAttributes redirectAttributes) {
+        boolean created = postService.create(post);
+
+        if (created) {
+            redirectAttributes.addFlashAttribute("successMessage", "게시글이 등록되었습니다.");
+            return "redirect:/posts";
+        }
+
+        redirectAttributes.addFlashAttribute("errorMessage", "게시글 등록에 실패했습니다.");
+        return "redirect:/post/create";
+    }
+}
